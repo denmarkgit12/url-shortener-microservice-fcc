1be8658caf6cc2da69f6675a9fdff109a94e735d
denmarkgit12 <denmark.work12@gmail.com>
2025-09-07 15:10:50 +0800

    Url Shortener

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +18,37 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/shorturl', (req,res)=> {
+
+  let urlDatabase = {};
+  let counter = 1;
+
+
+  const url = req.body.url;
+    const urlPattern = /^https?:\/\/.+/;
+        if (!urlPattern.test(originalUrl)) {
+        return res.json({ error: "invalid url" });
+  }
+
+  const shortUrl = counter++;
+  urlDatabase[shortUrl] = originalUrl;
+
+  res.json({  original_url: originalUrl,
+    short_url: shortUrl})
+
+})
+
+app.get("/api/shorturl/:short", (req, res) => {
+  const short = req.params.short;
+  const originalUrl = urlDatabase[short];
+
+  if (!originalUrl) {
+    return res.json({ error: "No short URL found for given input" });
+  }
+
+  res.redirect(originalUrl);
 });
 
 app.listen(port, function() {
