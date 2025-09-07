a10006ec5f46de05acdeb53d349d371c757eff4f
denmarkgit12 <denmark.work12@gmail.com>
2025-09-07 15:15:19 +0800

    Fix middleware

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
