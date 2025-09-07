03771cb4f774f829e04fa3d9b4f3afbde5597929
denmarkgit12 <denmark.work12@gmail.com>
2025-09-07 15:13:13 +0800

    Second Commit

--- index.js
+++ index.js
@@ -20,25 +20,22 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl', (req,res)=> {
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+  const urlPattern = /^https?:\/\/.+/;
 
-  let urlDatabase = {};
-  let counter = 1;
-
-
-  const url = req.body.url;
-    const urlPattern = /^https?:\/\/.+/;
-        if (!urlPattern.test(originalUrl)) {
-        return res.json({ error: "invalid url" });
+  if (!urlPattern.test(originalUrl)) {
+    return res.json({ error: "invalid url" });
   }
 
   const shortUrl = counter++;
   urlDatabase[shortUrl] = originalUrl;
 
-  res.json({  original_url: originalUrl,
-    short_url: shortUrl})
-
-})
+  res.json({
+    original_url: originalUrl,
+    short_url: shortUrl
+  });
+});
 
 app.get("/api/shorturl/:short", (req, res) => {
   const short = req.params.short;
